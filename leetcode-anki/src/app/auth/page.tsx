7efafe024060eb801fbd91efafb6321ea4a70050
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2024-05-27 18:05:04 +0700

    Add basic navbar styling

--- leetcode-anki/src/app/auth/page.tsx
+++ leetcode-anki/src/app/auth/page.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-   return <div>Auth page</div>;
+   return (
+      <div className="h-screen relative">
+         <div className="mx-auto">
+            <Navbar />
+         </div>
+      </div>
+   );
 };
 export default AuthPage;
--- leetcode-anki/src/components/Navbar/Navbar.tsx
+++ leetcode-anki/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-   return <div>Have a good coding</div>;
+   return (
+      <div className="flex justify-between items-center sm:px-12 md:px-24 px-4">
+         <Link href="/" className="flex items-center justify-center">
+            <h1 className="text-5xl">
+               <span className="text-brand-blue">Memorize</span> Code
+            </h1>
+         </Link>
+         <div className="flex items-center">
+            <button className="bg-brand-blue text-white px-4 py-4 rounded-lg">
+               Register
+            </button>
+         </div>
+      </div>
+   );
 };
 export default Navbar;
